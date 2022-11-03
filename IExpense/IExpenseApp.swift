b56de10202341718ca009a6f24daf5565f63a65b
romanmuni8 <romanmuni8@gmail.com>
2022-11-03 17:34:01 -0400

    Initial Commit

---
+++ IExpense/IExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IExpenseApp.swift
+//  IExpense
+//
+//  Created by Roman on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct IExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
